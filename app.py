a0a2af23eec8fa0256142856747fe7616f882116
JeroMu03 <88890795+JeroMu03@users.noreply.github.com>
2023-06-14 00:13:53 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for
+from flask import Flask, render_template, request, jsonify, redirect, url_for, session, flash
 from datetime import datetime
-#from models import db
-#from models import Asistencia, Curso, Estudiante, Padre, Preceptor
+from passver import PasswordVer
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
-
+from models import db
+from models import Asistencia, Curso, Estudiante, Padre, Preceptor
 
 @app.route('/', methods=['POST', 'GET'])
 def usuario():
@@ -15,11 +15,81 @@ def usuario():
 @app.route('/bienvenida', methods = ['POST', 'GET'])
 def bienvenida():
     if request.method == "POST":
-        if request.form['mail'] and request.form['contra']:
-            datosform=request.form
-            return render_template('bienvenida.html', datos=datosform)
+        if request.form['mail'] and request.form['contra'] and request.form['tipo']:
+            if request.form['tipo'] == 'padre':
+                padre = Padre.query.filter_by(correo=request.form['mail']).first()
+                if(padre is not None):
+                    print(padre)
+                    passver = PasswordVer(request.form['contra'])
+                    if(passver.validarPassword(padre.clave)):
+                        session["mail"] = padre.correo
+                        session["tipo"] = request.form['tipo']
+                        return render_template('menupadre.html', datos=[padre.nombre,padre.apellido])
+                flash('Verifica tus credenciales de acceso, Email o contraseña inválidos')
+                return render_template('login.html')
+            else:
+                preceptor = Preceptor.query.filter_by(correo=request.form['mail']).first()
+                if(type(preceptor) is not None):
+                    passver = PasswordVer(request.form['contra'])
+                    if(passver.validarPassword(preceptor.clave)):
+                        session["mail"] = preceptor.correo
+                        session["tipo"] = request.form['tipo']
+                        return render_template('menupreceptor.html', datos=[preceptor.nombre,preceptor.apellido])
+                flash('Verifica tus credenciales de acceso, Email o contraseña inválidos')
+                return render_template('login.html')
+        else:
+            flash('Verifica tus credenciales de acceso, DNI o contraseña inválidos')
+            return render_template('login.html')
     else:
         return render_template('login.html')
+    
+@app.route('/logout')
+def logout():
+    session.pop('mail')
+    session.pop('tipo')
+    return redirect(url_for('usuario'))
+
+#@app.route('/inasistencias')
+#def asistencia():
+#Fin Log out
+    
+#@app.route('/informe_detallado')
+#def informe_detallado():
+    #if 'user_id' not in session:
+        #return redirect('/login')
+
+    #estudiantes = Estudiante.query.order_by(Estudiante.apellido, Estudiante.nombre).all()
+    #informe = []
+
+    #for estudiante in estudiantes:
+        #asistencias_aula = Asistencia.query.filter_by(id_estudiante=estudiante.id, codigo= 1, asistio=True).count()
+        #asistencias_edfisica = Asistencia.query.filter_by(id_estudiante=estudiante.id, codigo= 2 , asistio=True).count()
+
+        #inasistencias_aula_justificadas = Asistencia.query.filter_by(id_estudiante=estudiante.id, codigo= 1 , asistio=False, justificacion.isnot(None)).count()
+        #inasistencias_aula_injustificadas = Asistencia.query.filter_by(id_estudiante=estudiante.id, codigo= 1 , asistio=False, justificacion.is_(None)).count()
+
+        #inasistencias_edfisica_justificadas = Asistencia.query.filter_by(id_estudiante=estudiante.id, codigo= 2 , asistio=False, justificacion.isnot(None)).count()
+        #inasistencias_edfisica_injustificadas = Asistencia.query.filter_by(id_estudiante=estudiante.id, codigo= 2 , asistio=False, justificacion.is_(None)).count()
+
+        #total_inasistencias = (
+            #inasistencias_aula_justificadas +
+            #inasistencias_aula_injustificadas +
+            #(inasistencias_edfisica_justificadas * 0.5) +
+            #(inasistencias_edfisica_injustificadas * 0.5) )
+
+        #informe.append({
+            #'estudiante': estudiante,
+            #'asistencias_aula': asistencias_aula,
+            #'asistencias_edfisica': asistencias_edfisica,
+            #'inasistencias_aula_justificadas': inasistencias_aula_justificadas,
+            #'inasistencias_aula_injustificadas': inasistencias_aula_injustificadas,
+            #'inasistencias_edfisica_justificadas': inasistencias_edfisica_justificadas,
+            #'inasistencias_edfisica_injustificadas': inasistencias_edfisica_injustificadas,
+            #'total_inasistencias': total_inasistencias })
+
+    #informe_ordenado = sorted(informe, key=lambda x: x['estudiante'].apellido)
+
+    #return render_template('informe_detallado.html', informe=informe_ordenado)
     
 if __name__ == '__main__': 
     app.run(debug = True)
--- models.py
+++ models.py
@@ -11,7 +11,6 @@ class Asistencia(db.Model):
     codigo = db.Column(db.Integer, nullable=False)
     asistio = db.Column(db.Boolean, nullable=False)
     justificacion = db.Column(db.String(50), nullable=False)
-    #idEstudiante = db.Column(db.Integer, nullable=False)
     id_estudiante = db.Column(db.Integer, db.ForeignKey('estudiante.id'))
     
 class Curso(db.Model):
@@ -19,9 +18,8 @@ class Curso(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     anio = db.Column(db.Integer, nullable=False)
     division = db.Column(db.Integer, nullable=False)
-    #idpreceptor = db.Column(db.Integer, nullable=False)
     id_preceptor = db.Column(db.Integer, db.ForeignKey('preceptor.id'))
-    estudiante = db.relationship('Estudiante',backref='curso',cascade='all, delete-orphan', lazy='dinamic')
+    estudiante = relationship('Estudiante',backref='curso',cascade='all, delete-orphan')
     
 class Estudiante(db.Model):
     __tablename__ = 'estudiante'
@@ -29,11 +27,9 @@ class Estudiante(db.Model):
     nombre = db.Column(db.String(50), nullable=False)
     apellido = db.Column(db.String(50), nullable=False)
     dni = db.Column(db.String(50), nullable=False)
-    #idcurso = db.Column(db.Integer, nullable=False)
-    #idpadre = db.Column(db.String(100), nullable=False)
     id_curso= db.Column(db.Integer, db.ForeignKey('curso.id'))
     id_padre = db.Column(db.Integer, db.ForeignKey('padre.id'))
-    asistencia_alum = db.relationship('Asistencia',backref='estudiante', cascade='all, delete-orphans', lazy='dinamic')
+    asistencia_alum = relationship('Asistencia',backref='estudiante', cascade='all, delete-orphan')
 
 class Padre(db.Model):
     __tablename__ = 'padre'
@@ -42,7 +38,7 @@ class Padre(db.Model):
     apellido = db.Column(db.String(50), nullable=False)
     correo = db.Column(db.String(100), nullable=False)
     clave = db.Column(db.String(100), nullable=False)
-    estudiantes = db.relationship('Estudiante',backref='padre', cascade='all, delete-orphans', lazy='dinamic')
+    estudiantes = relationship('Estudiante',backref='padre', cascade='all, delete-orphan')
     
 class Preceptor(db.Model):
     __tablename__ = 'preceptor'
@@ -51,4 +47,4 @@ class Preceptor(db.Model):
     apellido = db.Column(db.String(50), nullable=False)
     correo = db.Column(db.String(100), nullable=False)
     clave = db.Column(db.String(100), nullable=False)
-    cursos = db.relationship('Curso', backref='preceptor', cascade='all, delete-orphans', lazsy='dinamic')
+    cursos = relationship('Curso', backref='preceptor', cascade='all, delete-orphan')
